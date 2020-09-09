d37c8de701abaae937da6543e5a7d0f15b4e6449
Jeevesh Juneja <creativityinczenyoga@gmail.com>
2020-09-09 11:03:02 +0530

    Preserving checkpoints b/w runs and run-wise logs.

--- split_trainer.py
+++ split_trainer.py
@@ -7,14 +7,21 @@ import os
 import argparse
 from train import get_parser
 
-def replace_data_path(train_command, cur_dp) :
-    parts = train.command.split('--data_path')
-    assert len(parts) == 2
-    before_dp, after_dp = parts[0], parts[1]
-    after_parts_lis = [cur_dp+' ']+after_dp.split('--')[1:]
-    modified_command = before_dp+'--'.join(after_parts_lis)
+def replace_arg(train_command, cur_value, arg) :
+    parts = train_command.split(arg)
+    if len(parts)==1 :
+        parts.append('')
+    before_arg, after_arg = parts[0]+' '+arg+' ', parts[1]
+    after_arg_lis = [cur_value+' ']+after_arg.split('--')[1:]
+    modified_command = before_arg+'--'.join(after_arg_lis)
     return modified_command
 
+def get_arg(train_command, arg) :
+    parts = train_command.split(arg)
+    if len(parts)==1 :
+        return None
+    return parts[1].split('--')[0].rstrip().lstrip()
+ 
 def shift_files(source_dir, dest_dir, file_lis=[]) :
     shift_from = file_lis if len(file_lis)>0 else os.listdir(source_dir)
     shifted_files = []
@@ -23,10 +30,36 @@ def shift_files(source_dir, dest_dir, file_lis=[]) :
         shift_files.append(filename)
     return shifted_files
 
+def get_new_dump(train_command, i, delete_old) :
+ 
+    #Changing dump_path to that of next run
+    dump_path = get_arg(train_command, '--dump_path')
+    new_dump_path = os.path.join(os.path.split(dump_path)[0], 'run'+str(i))
+    train_command = replace_arg(train_command, new_dump_path, '--dump_path')
+    
+    if not os.path.isdir(new_dump_path) :
+        os.mkdirs(new_dump_path)
+    
+    #Changing checkpoint to load model from. 
+    dump_dir_dict = [f.path for f in os.scandir(path) if f.is_dir()]
+    dump_dirs.sort()
+    checkpoint_path = os.path.join(dump_dirs[0], 'checkpoint.pth')
+    assert os.path.isfile(checkpoint_path)
+    replace_arg(train_command, checkpoint_path, '--reload_model')
+    
+    #Removing useless directories of previous run. 
+    for j in range(1, len(dump_dirs)) :
+        command = 'rm -r '+dump_dirs[j]
+        if delete_old :
+            os.system(command)
+    
+    return train_command
 
+   
 parser = argparse.ArgumentParser()
 parser.add_argument('--command', help='Command to execute for training.')
-parser.add_argument('--n_reps', type=int, help='Number of times to loop through all splits. One split+common files are used in a single run')
+parser.add_argument('--n_reps', type=int, help='Number of times to loop through all splits. One split+common files are used in a single "run".')
+praser.add_argument('--delete_old', action='store_true', help='If this flag is provided, all folders in --dump_path except the one for the first GPU , are deleted for all the previous runs.')
 
 args = parser.parse_args()
 
@@ -37,23 +70,37 @@ train_command = '--'+train_command
 train_args = train_parser.parse(train_command)
 original_dp = train_args.data_path
 
+#Assigning new data path where all common files+files from one split will be stored, in each run.
+#This directory stays same the --data_path; new files are brought in and old files removed each run.
 cur_dp = './cur_data_path/'
-train_command = replace_data_path(train_command, cur_dp)
-
+train_command = replace_arg(train_command, cur_dp, '--data_path')
 if not os.path.isdir(cur_dp):
     os.mkdir(cur_dp)
-    
+
 common_files = shift_files(original_dp, cur_dp)
 
-for _ in range(args.n_reps) :
+#Making new run-wise dump paths for experiment. Nested within the dump_path provided in command.
+#E.g. :- --dump_path/run0/ , --dump_path/run1/ .. and so on.
+new_dump_path = os.path.join(get_arg(train_command, '--dump_path'), 'run0')
+train_command = replace_arg(train_command, new_dump_path, '--dump_path')
+if not os.path.isdir(new_dump_path) :
+    os.makedirs(new_dump_path)
+    
+tot_splits = 0
+for j in range(args.n_reps) :
     i=0
     while True :
         if i!=0 :
             shift_files(cur_dp, split_file_dir, split_i_files)
-        split_file_dir = os.path.join(original_dp, 'split'+str(i))
+        split_file_dir = os.path.join(original_dp, 'split'+str(i+j*tot_splits))
+        
         if not os.path.isdir(split_file_dir) :
+            tot_splits = i
             break
+        
         split_i_files = shift_files(split_file_dir, cur_dp)
+        if i!=0 :
+            train_command = get_new_dump(train_command, i, args.delete_old)
         os.system(train_command)
         i=i+1
     
