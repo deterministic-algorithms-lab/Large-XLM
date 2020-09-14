6ad0b6be4c0eeb901d5ea32b2e0621bb440c300b
Jeevesh8 <creativityinczenyoga@gmail.com>
2020-09-14 10:23:44 +0000

    Debugging split_trainer -- Kanpu Naki Made

--- split_trainer.py
+++ split_trainer.py
@@ -11,9 +11,10 @@ from utils.general import execute
 def replace_arg(train_command, cur_value, arg) :
     parts = train_command.split(arg)
     if len(parts)==1 :
+        parts[0] = parts[0]+' '
         parts.append('')
-    before_arg, after_arg = parts[0]+' '+arg+' ', parts[1]
-    after_arg_lis = [cur_value+' ']+after_arg.split('--')[1:]
+    before_arg, after_arg = parts[0]+arg, parts[1]
+    after_arg_lis = [' '+cur_value+' ']+after_arg.split('--')[1:]
     modified_command = before_arg+'--'.join(after_arg_lis)
     return modified_command
 
@@ -24,10 +25,11 @@ def get_arg(train_command, arg) :
     return parts[1].split('--')[0].rstrip().lstrip()
  
 def shift_files(source_dir, dest_dir, file_lis=[]) :
-    shift_from = file_lis if len(file_lis)>0 else os.listdir(source_dir)
+    shift_from = file_lis if len(file_lis)>0 else [f for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))]
     shifted_files = []
     for filename in  shift_from :
         command = "mv "+os.path.join(source_dir, filename)+' '+os.path.join(dest_dir, filename)
+        print(command)
         execute(command)
         shifted_files.append(filename)
     return shifted_files
@@ -40,36 +42,44 @@ def get_new_dump(train_command, i, delete_old) :
     train_command = replace_arg(train_command, new_dump_path, '--dump_path')
     
     if not os.path.isdir(new_dump_path) :
-        os.mkdirs(new_dump_path)
+        os.makedirs(new_dump_path)
     
     #Changing checkpoint to load model from. 
-    dump_dir_dict = [f.path for f in os.scandir(path) if f.is_dir()]
+    dump_dirs = [f.path for f in os.scandir(dump_path) if f.is_dir()]
     dump_dirs.sort()
-    checkpoint_path = os.path.join(dump_dirs[0], 'checkpoint.pth')
-    assert os.path.isfile(checkpoint_path)
-    replace_arg(train_command, checkpoint_path, '--reload_model')
+    checkpoint_path = os.path.join(dump_dirs[0] if len(dump_dirs)>0 else dump_path, 'checkpoint.pth')
+    #assert os.path.isfile(checkpoint_path)
+    train_command = replace_arg(train_command, checkpoint_path, '--reload_model')
     
     #Removing useless directories of previous run. 
     for j in range(1, len(dump_dirs)) :
         command = 'rm -r '+dump_dirs[j]
         if delete_old :
-            execute(command)
+           print(command)
+           execute(command)
     
     return train_command
 
    
 parser = argparse.ArgumentParser()
-parser.add_argument('--command', help='Command to execute for training.')
+parser.add_argument('--command', help='Path to file having the command to execute for training.')
 parser.add_argument('--n_reps', type=int, help='Number of times to loop through all splits. One split+common files are used in a single "run".')
-praser.add_argument('--delete_old', action='store_true', help='If this flag is provided, all folders in --dump_path except the one for the first GPU , are deleted for all the previous runs.')
+parser.add_argument('--delete_old', action='store_true', help='If this flag is provided, all folders in --dump_path except the one for the first GPU , are deleted for all the previous runs.')
 
 args = parser.parse_args()
 
+with open(args.command) as f :
+    train_command = f.read()
+    print(train_command)
+    train_command = train_command.strip()
+    train_command = train_command.replace('\\\n','')
+    print(train_command)
+
 train_parser = get_parser()
-train_command = args.command
-initial_command, train_command = train_command.split('--', 1)
+initial_command, train_command = train_command.split('--',1)
 train_command = '--'+train_command
-train_args = train_parser.parse(train_command)
+print(train_command)
+train_args = train_parser.parse_args(train_command.split())
 original_dp = train_args.data_path
 
 #Assigning new data path where all common files+files from one split will be stored, in each run.
@@ -94,16 +104,20 @@ for j in range(args.n_reps) :
     while True :
         if i!=0 :
             shift_files(cur_dp, split_file_dir, split_i_files)
-        split_file_dir = os.path.join(original_dp, 'split'+str(i+j*tot_splits))
+        split_file_dir = os.path.join(original_dp, 'split'+str(i))
         
         if not os.path.isdir(split_file_dir) :
-            tot_splits = i
+            if j==0 :
+                tot_splits = i
             break
         
         split_i_files = shift_files(split_file_dir, cur_dp)
-        if i!=0 :
-            train_command = get_new_dump(train_command, i, args.delete_old)
-        execute(train_command)
+        if i!=0 or j!=0 :
+            train_command = get_new_dump(train_command, i+j*tot_splits, args.delete_old)
+            
+        final_command = initial_command+train_command
+        print(final_command)
+        execute(final_command)
         i=i+1
     
 shift_files(cur_dp, original_dp, common_files)
